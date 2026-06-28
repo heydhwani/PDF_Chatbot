37d7427edeb3d9c6b7d3034059e89089af07f4c6
Dhwani <dhwanijain731@gmail.com>
2026-06-28 10:44:18 +0530

    context retrieved

--- rag/retriever.py
+++ rag/retriever.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from api import client
 
+
+
 def load_vectorstore():
 
     index = faiss.read_index(
@@ -18,3 +20,43 @@ def load_vectorstore():
         chunks = pickle.load(file)
 
     return index, chunks
+
+
+
+def question_embedding(question):
+
+    response = client.models.embed_content(
+
+        model="text-embedding-004",
+
+        contents=question
+
+    )
+
+    embedding = response.embeddings[0].values
+
+    return np.array(
+        [embedding],
+        dtype=np.float32
+    )
+
+
+
+def retrieve_context(question, k=3):
+
+    index, chunks = load_vectorstore()
+
+    question_vector = question_embedding(question)
+
+    distances, indices = index.search(
+        question_vector,
+        k
+    )
+
+    context = []
+
+    for i in indices[0]:
+
+        context.append(chunks[i])
+
+    return "\n\n".join(context)
